5f375bbade7e2f5519d26d9c9739e0d5756e1759
Олег Диденко <diaglyonok@yandex.ru>
2025-09-07 00:45:58 +0500

    fixed parsing

--- lib/widget/dynamic_form.dart
+++ lib/widget/dynamic_form.dart
@@ -1107,7 +1107,7 @@ class DynamicFormState extends State<DynamicForm> {
       initialLightness: color == null ? 0.5 : HSLColor.fromColor(color).lightness,
       field: field,
       onChanged: (color) {
-        _commonOnChanged(CompositeValue(color.toString()), field);
+        _commonOnChanged(CompositeValue(color.toARGB32().toString()), field);
       },
     );
   }
